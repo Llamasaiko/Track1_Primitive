7843dbe84ae256feec74b22068768a9eb98ac283
Frank <liyinqiu007@gmail.com>
2017-11-15 07:02:21 -0600

    updated_models
    hotel_models

--- mysite/reviews/models.py
+++ mysite/reviews/models.py
@@ -21,7 +21,8 @@ from django.db import models
 
 class Review_Entity(models.Model):
     rid = models.IntegerField(primary_key=True, blank=False, null=False)
-    hid = models.IntegerField(blank=False, null=False)
+    hid = models.IntegerField(blank=False, null=True)
+ #   hid = models.ForeignKey('Hotel_Entity', on_delete=models.CASCADE, blank=False, null=False)
     Review_Date = models.CharField(max_length=15)
     Reviewer_Nationality = models.CharField(max_length=50)
     Positive_Review = models.TextField(max_length=2000)
@@ -29,5 +30,14 @@ class Review_Entity(models.Model):
     Review_Total_Positive_Word_Counts = models.IntegerField(blank=True, null=True)
     Review_Total_Negative_Word_Counts = models.IntegerField(blank=True, null=True)
     Total_Number_of_Reviews_Reviewer_Has_Given = models.IntegerField(blank=True, null=True)
-    Reviewer_Score = models.FloatField(blank=True, null=True)
+    Reviewer_Score = models.DecimalField(max_digits=3,decimal_places=1,blank=True, null=True)
     Tags = models.TextField(blank=True, null=True)
+class Hotel_Entity(models.Model):
+    Hotel_Name = models.CharField(max_length=100,blank=True,null=True)
+    Hotel_Address = models.CharField(max_length=200,blank=True,null=True)
+    lat = models.DecimalField(max_digits=10,decimal_places=7,blank=True,null=True)
+    lng = models.DecimalField(max_digits=10,decimal_places=7,,blank=True,null=True)
+    Average_Score = models.DecimalField(max_digits=3,decimal_places=1,,blank=True,null=True)
+    Total_Number_of_Reviews = models.IntegerField(blank=True,null=True)
+    Additional_Number_of_Scoring = models.IntegerField(blank=True,null=True)
+    hid = models.IntegerField(primary_key=True,Serialized=False)
